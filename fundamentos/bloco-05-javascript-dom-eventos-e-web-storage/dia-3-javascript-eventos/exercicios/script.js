95e404ca1b8191b4f5d37cae7d65b3de8dba921d
Fa-biano <fbiano.souza@gmail.com>
2022-06-30 23:14:49 -0300

    adicionado resolucao calendario trybe exercicios 2 e 3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
@@ -3,6 +3,7 @@ let holidays = [24, 25, 31];
 let fridays = [4, 11, 18, 25];
 let ulOfDays = document.getElementById('days');
 
+// Exercício 1:
 function createCalendarDays() {
     for (let day of decemberDaysList) {
         ulOfDays.innerHTML += '<li>' + day;
@@ -26,3 +27,31 @@ function setClassName(paramDays, addClass) {
 setClassName(decemberDaysList, 'day');
 setClassName(holidays, 'holiday');
 setClassName(fridays, 'friday');
+
+// Exercício 2:
+let buttonsContainer = document.querySelector('.buttons-container');
+let buttonTag = document.createElement('button');
+
+function createButton(parent, child) {
+    parent.appendChild(child);
+    parent.firstElementChild.id = 'btn-holiday';
+    parent.firstElementChild.innerText = 'Feriados';
+}
+
+createButton(buttonsContainer, buttonTag);
+
+// Exercício 3:
+function holidayHighlight() {
+    let holidays = document.querySelectorAll('.holiday');
+    for (let holiday of holidays) {
+        
+        if (holiday.style.backgroundColor === 'red'){
+            holiday.style.backgroundColor = 'rgb(238,238,238)'
+        } else {
+            holiday.style.backgroundColor = 'red';
+        }
+    }
+}
+
+buttonsContainer.firstElementChild.addEventListener('click', holidayHighlight)
+
