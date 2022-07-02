1c3a8494a530a88867cb520e339f0b0e0b111f4b
Fa-biano <fbiano.souza@gmail.com>
2022-07-01 23:00:04 -0300

    criadas as funcoes para customizar a cor do texto e background

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,10 +1,44 @@
-let bgColorOption = document.querySelectorAll('.circle');
-function changeBgColor() {
+let colorOption = document.querySelectorAll('.circle');
+let body = document.body
 
-  // for (let index = 0; index < 3; index += 1) {
-  //   if (bgColorOption[index])
-  // }
-  console.log('Clicouuuu');
+function changeBgColor(event) {
+  let setBgColor
+  for (let index = 0; index < 3; index += 1) {
+    if (event.target === colorOption[index]) {
+      if (index === 0) {
+        setBgColor = 'black'
+      } else if (index === 1) {
+        setBgColor = 'white'
+      } else {
+        setBgColor = 'blue'
+      }
+    }
+  }
+
+  body.style.backgroundColor = setBgColor;
 }
 
-bgColorOption[0].addEventListener('click', changeBgColor)
+colorOption[0].addEventListener('click', changeBgColor)
+colorOption[1].addEventListener('click', changeBgColor)
+colorOption[2].addEventListener('click', changeBgColor)
+
+function changeColorText(event) {
+  let setColorText
+  for (let index = 3; index < 6; index += 1) {
+    if (event.target === colorOption[index]) {
+      if (index === 3) {
+        setColorText = 'black'
+      } else if (index === 4) {
+        setColorText = 'white'
+      } else {
+        setColorText = 'blue'
+      }
+    }
+  }
+  
+  body.style.color = setColorText;
+}
+
+colorOption[3].addEventListener('click', changeColorText)
+colorOption[4].addEventListener('click', changeColorText)
+colorOption[5].addEventListener('click', changeColorText)
