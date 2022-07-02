8ccabc6a7bc5bddad069f9c647591d775d06ef77
Fa-biano <fbiano.souza@gmail.com>
2022-07-01 22:27:29 -0300

    criados arquivos e configuracoes iniciais  de index + style + script

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -0,0 +1,10 @@
+let bgColorOption = document.querySelectorAll('.circle');
+function changeBgColor() {
+
+  // for (let index = 0; index < 3; index += 1) {
+  //   if (bgColorOption[index])
+  // }
+  console.log('Clicouuuu');
+}
+
+bgColorOption[0].addEventListener('click', changeBgColor)
