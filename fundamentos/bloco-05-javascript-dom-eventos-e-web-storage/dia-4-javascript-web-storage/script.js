907e843d2ae853e88c3f5c5f58cd96e29633233b
Fa-biano <fbiano.souza@gmail.com>
2022-07-02 00:13:28 -0300

    criadas as funcoes para customizar tamanho e tipo da fonte + altura da linha

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,16 +1,21 @@
-let colorOption = document.querySelectorAll('.circle');
 let body = document.body
+let colorOption = document.getElementById('bg-color');
+let colorTextOption = document.getElementById('color-text');
+let fontSizeOptions = document.getElementById('font-size');
+let lineHeightOptions = document.getElementById('line-height');
+let paragraghs = document.getElementsByTagName('p')
+let fontFamilyOptions = document.getElementById('font-family');
 
 function changeBgColor(event) {
   let setBgColor
-  for (let index = 0; index < 3; index += 1) {
-    if (event.target === colorOption[index]) {
+  for (let index = 0; index < colorOption.children.length; index += 1) {
+    if (event.target === colorOption.children[index]) {
       if (index === 0) {
-        setBgColor = 'black'
+        setBgColor = 'black';
       } else if (index === 1) {
-        setBgColor = 'white'
+        setBgColor = 'white';
       } else {
-        setBgColor = 'blue'
+        setBgColor = 'blue';
       }
     }
   }
@@ -18,27 +23,77 @@ function changeBgColor(event) {
   body.style.backgroundColor = setBgColor;
 }
 
-colorOption[0].addEventListener('click', changeBgColor)
-colorOption[1].addEventListener('click', changeBgColor)
-colorOption[2].addEventListener('click', changeBgColor)
-
 function changeColorText(event) {
   let setColorText
-  for (let index = 3; index < 6; index += 1) {
-    if (event.target === colorOption[index]) {
-      if (index === 3) {
+  for (let index = 0; index < colorTextOption.children.length; index += 1) {
+    if (event.target === colorTextOption.children[index]) {
+      if (index === 0) {
         setColorText = 'black'
-      } else if (index === 4) {
+      } else if (index === 1) {
         setColorText = 'white'
       } else {
         setColorText = 'blue'
       }
     }
-  }
+  } 
   
   body.style.color = setColorText;
 }
 
-colorOption[3].addEventListener('click', changeColorText)
-colorOption[4].addEventListener('click', changeColorText)
-colorOption[5].addEventListener('click', changeColorText)
+function changeFontSize(event) {
+  let setTextSize
+  for (let index = 0; index < fontSizeOptions.children.length; index += 1) {
+    if (event.target === fontSizeOptions.children[index]) {
+      if (index === 0) {
+        setTextSize = '0.8em'
+      } else if (index === 1) {
+        setTextSize = '1.0em'
+      } else {
+        setTextSize = '1.5em'
+      }
+    }
+  }
+  body.firstElementChild.nextElementSibling.style.fontSize = setTextSize;
+}
+
+function changeLineHeight(event) {
+  let setLineHeight
+  for (let index = 0; index < lineHeightOptions.children.length; index += 1) {
+    if (event.target === lineHeightOptions.children[index]) {
+      if (index === 0) {
+        setLineHeight = '16px'
+      } else if (index === 1) {
+        setLineHeight = '25px'
+      } else {
+        setLineHeight = '32px'
+      }
+    }
+  }
+
+  for (let paragragh of paragraghs) {
+    paragragh.style.lineHeight = setLineHeight
+  }
+}
+
+function changeFontFamily(event) {
+  let setFontFamily
+  for (let index = 0; index < fontFamilyOptions.children.length; index += 1) {
+    if (event.target === fontFamilyOptions.children[index]) {
+      if (index === 0) {
+        setFontFamily = 'Arial'
+      } else if (index === 1) {
+        setFontFamily = '"Times New Roman"'
+      } else {
+        setFontFamily = 'cursive'
+      }
+    }
+  }
+  body.children[0].style.fontFamily = setFontFamily;
+  body.children[1].style.fontFamily = setFontFamily;
+}
+
+colorOption.addEventListener('click', changeBgColor);
+colorTextOption.addEventListener('click', changeColorText);
+fontSizeOptions.addEventListener('click', changeFontSize);
+lineHeightOptions.addEventListener('click', changeLineHeight);
+fontFamilyOptions.addEventListener('click', changeFontFamily);
