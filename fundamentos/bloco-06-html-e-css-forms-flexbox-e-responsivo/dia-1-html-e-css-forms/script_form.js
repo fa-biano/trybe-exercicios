6f1ad7665eb0582d711a1f6818655e29a4b92c2b
Fa-biano <fbiano.souza@gmail.com>
2022-07-06 20:54:02 -0300

    exercicio pratico: adicionado arquivo com estilo + script com validacoes

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/script_form.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/script_form.js
@@ -1,4 +1,17 @@
 let btnSubmit = document.getElementById('btn-submit');
+let auth = document.getElementById('autorizacao2');
+let nameSize = document.getElementById('nome-completo');
+let emailSize = document.getElementById('email');
+
 btnSubmit.addEventListener('click', (event) => {
-  event.preventDefault();
+  if (nameSize.value.length < 10 || emailSize.value.length < 10) {
+    alert('Dados Inválidos')
+    event.preventDefault();
+  } else if (auth.checked !== true){
+    alert('Necessário marcar a opção que autoriza a utilização das imagens')
+    event.preventDefault();
+  } else {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+  }
 })
+
