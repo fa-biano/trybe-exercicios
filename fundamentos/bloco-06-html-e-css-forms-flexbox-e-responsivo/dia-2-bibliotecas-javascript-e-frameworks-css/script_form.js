97adfc0afd9cc4a2891b351bc5dc9a8be68bf94a
Fa-biano <fbiano.souza@gmail.com>
2022-07-07 21:32:05 -0300

    adicionadas bibiliotecas para manipulacao de datas e validao dos campos nome e email

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script_form.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script_form.js
@@ -5,13 +5,14 @@ let emailSize = document.getElementById('email');
 
 btnSubmit.addEventListener('click', (event) => {
   if (nameSize.value.length < 10 || emailSize.value.length < 10) {
-    alert('Dados Inválidos')
-    event.preventDefault();
+    // alert('Dados Inválidos')
+    // event.preventDefault();
   } else if (auth.checked !== true){
     alert('Necessário marcar a opção que autoriza a utilização das imagens')
     event.preventDefault();
   } else {
     alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+    event.de
   }
 })
 
