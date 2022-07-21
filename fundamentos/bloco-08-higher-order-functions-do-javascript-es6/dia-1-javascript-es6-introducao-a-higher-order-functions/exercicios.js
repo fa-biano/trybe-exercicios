bdb10c7dc9d9ba484a9e8ef65d2781dff777ea50
Fa-biano <fbiano.souza@gmail.com>
2022-07-21 18:40:26 -0300

    exercicios: realiza o exercio 3

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
@@ -22,7 +22,39 @@ const numPrize = () => Math.ceil(Math.random() * 5);
 
 const matchPrize = (func, num) => {
   const prize = func();
-  return num === prize ? "Parabéns você ganhou": "Tente novamente";
+  return num === prize ? "Parabéns você ganhou" : "Tente novamente";
 }
 
 console.log(matchPrize(numPrize, 5));
+
+// 3 - Crie uma HOF que receberá três parâmetros:
+// O primeiro será um array de respostas corretas (Gabarito);
+// O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
+// O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. Para isso essa função usará os seguintes critérios:
+// Uma resposta correta adiciona 1 ponto à pontuação final;
+// A ausência de uma resposta não altera a pontuação (quando for "N.A");
+// Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
+// Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays:
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const checkAnswer = (correctAnswer, inpuntAnswer) => {
+  if (correctAnswer.length === inpuntAnswer.length) {
+    let score = 0;
+    for (let index = 0; index < correctAnswer.length; index += 1) {
+      if (inpuntAnswer[index] === correctAnswer[index]) {
+        score += 1;
+      } else if (inpuntAnswer[index] === 'N.A') {
+        score += 0;
+      } else {
+        score -= 0.5;
+      }
+    }
+    return (score >= 0) ? score : 0;
+  }
+}
+
+const answerRate = (correctAnswer, inpuntAnswer, analyzer) => analyzer(correctAnswer, inpuntAnswer);
+
+console.log(answerRate(RIGHT_ANSWERS, STUDENT_ANSWERS, checkAnswer));
