b30532432c0767afad0e39dd467caf71a3493736
Fa-biano <fbiano.souza@gmail.com>
2022-12-06 21:58:33 -0300

    exercicios agora a pratica: 5

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/bmi-interactive.js
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/bmi-interactive.js
@@ -1,6 +1,6 @@
 const readline = require('readline-sync');
 
-const weightInKg = readline.questionInt('What is your weight? (Kg) ');
+const weightInKg = readline.questionFloat('What is your weight? (Kg) ');
 const heightInMeters = readline.questionFloat('What is your height? (Meters) ');
 
 const handeBMI = (weight, height) => {
@@ -13,9 +13,53 @@ const handeBMI = (weight, height) => {
   return bmi;
 };
 
+const BMI_MAX_AND_MIN = {
+  'Underweight': {
+    minBMI: 0,
+    maxBMI: 18.4,
+  },
+  'Normal Weight': {
+    minBMI: 18.5,
+    maxBMI: 24.9,
+  },
+  'Overweight': {
+    minBMI: 25,
+    maxBMI: 29.9,
+  },
+  'Obese Class I': {
+    minBMI: 30.0,
+    maxBMI: 34.9,
+  },
+  'Obese Class II': {
+    minBMI: 35,
+    maxBMI: 39.9,
+  },
+  'Obese Class III': {
+    minBMI: 40,
+    maxBMI: 100,
+  },
+};
+
+const calcBmiStatus = (bmi) => {
+  const status  = Object.keys(BMI_MAX_AND_MIN);
+
+  const findStatus = status.find((stat) => {
+    const { minBMI, maxBMI } = BMI_MAX_AND_MIN[stat];
+    const isMinBMI = minBMI ? bmi > minBMI : true;
+    const isMaxBMI = maxBMI ? bmi < maxBMI : true;
+
+    return isMinBMI && isMaxBMI;
+  })
+
+  return findStatus;
+};
+
 const main = () => {
   const bmi = handeBMI(weightInKg, heightInMeters).toFixed(2);
-  console.log(`Your BMI(Body Mass Index) is ${bmi}`);
+  const status = calcBmiStatus(bmi);
+
+
+  console.log(`Your BMI(Body Mass Index) is ${bmi}. And Your BMI Status is ${status}.`);
 }
 
 main();
