e7ed8adc5c2056fb1fa4f4feb251b64f5abfc6e2
Fa-biano <fbiano.souza@gmail.com>
2022-12-07 23:42:56 -0300

    exercicios fixacao: 4 - cria funcao para leitura de arquivo

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-fixacao/src/app.js
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-fixacao/src/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(`Arquivo não pôde ser lido: ${err}`);
+  }
+}
 
 module.exports = { app };
