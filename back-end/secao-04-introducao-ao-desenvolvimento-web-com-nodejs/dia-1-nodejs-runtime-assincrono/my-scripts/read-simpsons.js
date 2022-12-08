612a071cbf40ceb8377210dbef85b314b52c7df1
Fa-biano <fbiano.souza@gmail.com>
2022-12-07 23:08:06 -0300

    exercicios agora a pratica: 6C ao 6F

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/read-simpsons.js
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/read-simpsons.js
@@ -19,11 +19,43 @@ const getSimpsonById = async (id) => {
   return chosenSimpson
 }
 
+const filterSimpson = async () => {
+  const simpsons = await readAll();
+  const newArray = simpsons.filter((simpson) => simpson.id !== '10' && simpson.id !== '6');
+  await fs.writeFile('./simpsons.json', JSON.stringify(newArray));
+};
+
+const createSimpsonsFamily = async () => {
+  const simpsons = await readAll();
+  const familyIds = [1, 2, 3, 4];
+  const simpsonsFamily = simpsons.filter((simpson) => familyIds.includes(Number(simpson.id)));
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsFamily));
+};
+
+const addNelsonToFamily = async () => {
+  const fileContent = await fs.readFile('./simpsonFamily.json', 'utf-8');
+  const simpsonsFamily  = JSON.parse(fileContent); 
+  simpsonsFamily.push({ id: '8', name: 'Nelson Muntz' });
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsFamily));
+};
+
+const switchNelsonToMaggie = async () => {
+  const fileContent = await fs.readFile('./simpsonFamily.json', 'utf-8');
+  const simpsonsFamily  = JSON.parse(fileContent);
+  const removeNelson = simpsonsFamily.filter((simpson) => simpson.id !== '8');
+  const addMaggieToFamily = [ ...removeNelson, { id: '5', name:'Maggie Simpson' } ];
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(addMaggieToFamily));
+};
+
 
 const main = async () => {
   await getAllSimpsons();
   const simpson = await getSimpsonById(1);
   console.log(simpson);
+  await filterSimpson();
+  await createSimpsonsFamily();
+  await addNelsonToFamily();
+  await switchNelsonToMaggie();
 };
 
 main();
