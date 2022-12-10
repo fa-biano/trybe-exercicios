9f5e2e9fb89e535cfb330dab8d28d2e9af91fad4
Fa-biano <fbiano.souza@gmail.com>
2022-12-10 14:43:42 -0300

    finalizados os testes, rotas e funcoes de manipulacao dos dados

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacau-trybe/src/app.js
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-nodejs-testes-de-integracao/cacau-trybe/src/app.js
@@ -5,6 +5,19 @@ const cacauTrybe = require('../src/cacauTrybe');
 app.get('/chocolate', async (req, res) => {
   const chocolates = await cacauTrybe.getAllChocolates();
   res.status(200).json({ chocolates });
-})
+});
+
+app.get('/chocolate/:id', async (req, res) => {
+  const { id } = req.params;
+  const chocolate = await cacauTrybe.getChocolateById(Number(id));
+  if (!chocolate) return res.status(404).json({ message: 'Chocolate not found' });
+  return res.status(200).json({ chocolate });
+});
+
+app.get('/chocolate/brand/:brandId', async (req, res) => {
+  const { brandId  } = req.params;
+  const chocolates = await cacauTrybe.getChocolatesByBrand(Number(brandId));
+  return res.status(200).json({ chocolates });
+});
 
 module.exports = app;
