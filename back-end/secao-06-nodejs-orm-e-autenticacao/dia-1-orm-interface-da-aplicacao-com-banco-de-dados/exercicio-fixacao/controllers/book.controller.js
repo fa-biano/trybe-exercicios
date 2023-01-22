eee89ccbb58704b53a918f231372e0f8b60a9963
Fa-biano <fbiano.souza@gmail.com>
2023-01-22 19:50:00 -0300

    exercicio: 6, 7 e 8 buscando um livro pelo id

--- back-end/secao-06-nodejs-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-banco-de-dados/exercicio-fixacao/controllers/book.controller.js
+++ back-end/secao-06-nodejs-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-banco-de-dados/exercicio-fixacao/controllers/book.controller.js
@@ -1,15 +1,31 @@
 const bookService = require('../services/book.service');
 
+const errorMsg = 'Algo deu errado';
+
 const getAll = async (_req, res) => {
   try {
     const books = await bookService.getAll();
     return res.status(200).json(books);
   } catch (e) {
     console.log(e.message);
-    return res.status(500).json({ message: 'Algo deu errado' });
+    return res.status(500).json({ message: errorMsg });
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await bookService.getById(id);
+
+    if (!book) return res.status(404).json({ message: 'book not found' });
+    return res.status(200).json(book);
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ message: errorMsg });
+  }
+}
+
 module.exports = {
   getAll,
+  getById,
 };
--- back-end/secao-06-nodejs-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-banco-de-dados/exercicio-fixacao/index.js
+++ back-end/secao-06-nodejs-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-banco-de-dados/exercicio-fixacao/index.js
@@ -8,5 +8,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/books', Book.getAll);
+app.get('/books/:id', Book.getById);
 
 app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
