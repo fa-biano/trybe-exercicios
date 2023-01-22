de61238745136f0350e624272a7d01d6133032ea
Fa-biano <fbiano.souza@gmail.com>
2023-01-22 20:49:31 -0300

    exercicio bonus: adicionada uma coluna na tabela books

--- back-end/secao-06-nodejs-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-banco-de-dados/exercicio-fixacao/controllers/book.controller.js
+++ back-end/secao-06-nodejs-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-banco-de-dados/exercicio-fixacao/controllers/book.controller.js
@@ -2,9 +2,17 @@ const bookService = require('../services/book.service');
 
 const errorMsg = 'Algo deu errado';
 
-const getAll = async (_req, res) => {
+const getAll = async (req, res) => {
   try {
-    const books = await bookService.getAll();
+    const { author } = req.query;
+    let books
+
+    if (author) {
+      books = await bookService.getByAuthor(author);
+    } else {
+      books = await bookService.getAll();
+    }
+
     return res.status(200).json(books);
   } catch (e) {
     console.log(e.message);
@@ -27,8 +35,8 @@ const getById = async (req, res) => {
 
 const createBook = async (req, res) => {
   try {
-    const { title, author, pageQuantity } = req.body;
-    const newBook = await bookService.createBook(title, author, pageQuantity);
+    const { title, author, pageQuantity, publisher } = req.body;
+    const newBook = await bookService.createBook(title, author, pageQuantity, publisher);
     return res.status(201).json(newBook);
   } catch (e) {
     console.log(e.message);
@@ -39,9 +47,9 @@ const createBook = async (req, res) => {
 const updateBook = async (req, res) => {
   try {
     const { id } = req.params;
-    const { title, author, pageQuantity } = req.body;
+    const { title, author, pageQuantity, publisher } = req.body;
 
-    const book = await bookService.updateBook(id, title, author, pageQuantity);
+    const book = await bookService.updateBook(id, title, author, pageQuantity, publisher);
 
     if (!book) return res.status(404).json({ message: 'book not found'});
     return res.status(200).json({ message: 'book updated sucessfully!'});
