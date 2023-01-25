5312427edc9a777e87de16b929101b479efef2ca
Fa-biano <fbiano.souza@gmail.com>
2023-01-25 20:40:27 -0300

    corrige exercicio 1 + exercicio 2 done

--- back-end/secao-06-nodejs-orm-e-autenticacao/dia-4-jwt-json-web-token/hello-jwt/hello-jwt/controllers/login.js
+++ back-end/secao-06-nodejs-orm-e-autenticacao/dia-4-jwt-json-web-token/hello-jwt/hello-jwt/controllers/login.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = process.env;
 
-const validateBody = (body) => {
+const validateBody = (body) =>
   Joi.object({
     username: Joi.string().min(5).alphanum().required().messages({
       'string.min': '"username" length must be 5 characters long',
@@ -14,16 +14,23 @@ const validateBody = (body) => {
       'string.required': '"password" is required',
     }),
   }).validate(body);
-};
 
 module.exports = async (req, res, next) => {
   const { error } = validateBody(req.body);
 
   if (error) return next(error)
 
+  if (req.body.username === 'admin' && req.body.password !== 's3nh4S3gur4???') {
+    const err = new Error('Invalid username or password');
+    err.statusCode = 401
+    return next(err);
+  }
+
+  const admin = req.body.username === 'admin' && req.body.password === 's3nh4S3gur4???';
+
   const payload = {
     username: req.body.username,
-    admin: false,
+    admin,
   };
 
   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
