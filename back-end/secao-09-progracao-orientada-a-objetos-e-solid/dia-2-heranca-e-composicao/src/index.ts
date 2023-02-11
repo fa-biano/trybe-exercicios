21df2d1197625d8e9ce7da262e91836882e765c8
Fa-biano <fbiano.souza@gmail.com>
2023-02-10 21:39:10 -0300

    exercicio 1

---
+++ back-end/secao-09-progracao-orientada-a-objetos-e-solid/dia-2-heranca-e-composicao/src/index.ts
@@ -0,0 +1,8 @@
+import Person from "./Person";
+
+const erza = new Person('Erza', new Date('1990/11/02'));
+const natsu = new Person('Natsu', new Date('2000/11/02'));
+
+console.log(erza);
+console.log(natsu);
+
