aebfaf48a46d003fb53209694f8595fe81ba7dd0
Fa-biano <fbiano.souza@gmail.com>
2023-06-05 19:26:09 -0300

    exercicios

---
+++ ciencia-da-computacao/secao-05-estrutura-de-dados-1-arrays-listas-filas-pilhas/dia-5-stacks-e-queues/exercicio1_queue.py
@@ -0,0 +1,60 @@
+# crie uma classe Queue, onde deve conter as operações:
+# enqueue, dequeue, peek e is_empty.
+
+
+class Queue():
+    def __init__(self):
+        self._data = list()
+
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return not bool(self.size())
+
+    def enqueue(self, value):
+        self._data.append(value)
+
+    def dequeue(self):
+        if len(self._data) == 0:
+            return None
+        return self._data.pop(0)
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        value = self._data[0]
+        return value
+
+    def __str__(self):
+        str_items = ""
+        for i in range(len(self._data)):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < len(self._data):
+                str_items += ", "
+
+        return "Queue(" + str_items + ")"
+
+
+if __name__ == "__main__":
+    elements = ["Milkshake", "Batata Frita", "Refrigerante"]
+    content_queue = Queue()
+
+    print(content_queue)
+    # Saída: Queue()
+
+    for elem in elements:
+        content_queue.enqueue(elem)
+
+    print(content_queue)
+    # Saída: Queue(Milkshake, Batata Frita, Refrigerante)
+
+    content_queue.dequeue()
+    print(content_queue)
+    # Saída: Queue(Batata Frita, Refrigerante)
+
+    print(content_queue.dequeue())
+    # Saída: Batata Frita
+    # porque "Milkshake" já foi removido e "Batata Frita" se tornou o primeiro
+    # elemento da fila
