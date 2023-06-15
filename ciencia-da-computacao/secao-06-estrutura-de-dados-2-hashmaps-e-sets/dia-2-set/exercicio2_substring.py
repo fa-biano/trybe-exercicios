4973f7015ecb3506c3795038e95b0744bd3dcbc5
Fa-biano <fbiano.souza@gmail.com>
2023-06-15 17:44:52 -0300

    exercicios

---
+++ ciencia-da-computacao/secao-06-estrutura-de-dados-2-hashmaps-e-sets/dia-2-set/exercicio2_substring.py
@@ -0,0 +1,18 @@
+# Dada uma string, ache o tamanho da maior substring que não tenha caracteres
+# repetidos. Complexidade de tempo limite aceitável: O(n²).
+
+def longer_no_repeating_substring_len(string):
+    biggest = 0
+    for index, _ in enumerate(string):
+        substr = set()
+        for letter in string[index:]:
+            if letter in substr:
+                break
+            substr.add(letter)
+        if len(substr) > biggest:
+            biggest = len(substr)
+    return biggest
+
+
+string = "serdevemuitolegalmasehprecisoestudarbastante"
+print(longer_no_repeating_substring_len(string))
